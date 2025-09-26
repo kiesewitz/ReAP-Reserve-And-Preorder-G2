5150d29fc868eebb82e4a2e600b50d321bff263a
kiesewitz <kiesewitz@gmail.com>
2025-09-26 09:01:20 +0200

    Add blank slate Spring Boot project

---
+++ Reserve_And_Preorder_G2/src/main/java/at/htlle/reserve_and_preorder_g2/ReserveAndPreorderG2Application.java
@@ -0,0 +1,13 @@
+package at.htlle.reserve_and_preorder_g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveAndPreorderG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveAndPreorderG2Application.class, args);
+	}
+
+}
---
+++ Reserve_And_Preorder_G2/src/test/java/at/htlle/reserve_and_preorder_g2/ReserveAndPreorderG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlle.reserve_and_preorder_g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveAndPreorderG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
