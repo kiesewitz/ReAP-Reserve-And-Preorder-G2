fc0d48886f428a75a6a0bd11712735fd5d298bcd
KaiPrf <211witb20@o365.htl-leoben.at>
2025-10-17 10:39:52 +0200

    Owner App hinzugefügt

---
+++ Reserve_And_Preorder_Owner_G2/src/main/java/at/htlle/reap/ReserveAndPreorderChefG2Application.java
@@ -0,0 +1,13 @@
+package at.htlle.reap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveAndPreorderChefG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveAndPreorderChefG2Application.class, args);
+	}
+
+}
---
+++ Reserve_And_Preorder_Owner_G2/src/test/java/at/htlle/reap/ReserveAndPreorderChefG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlle.reap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveAndPreorderChefG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
